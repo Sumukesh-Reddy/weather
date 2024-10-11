3870323ae3ca82e7cb476aa6842af3e79435f644
Sumukesh <sumukeshmopuram1@gmail.com>
2024-10-11 12:10:16 +0530

    I have chnaged in js to decrease erro,so i have addded catch and some more

--- weatherapp.js
+++ weatherapp.js
@@ -1,28 +1,60 @@
- let weather={
-    "api":"337e822ad82ee3693661794c6ca440b2",
-    fetchweather: function (city){
-        fetch("https://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&appid=337e822ad82ee3693661794c6ca440b2")
-        .then((response)=>response.json())
-        .then((data)=>this.displayweather(data))
-    } ,
-    displayweather: function (data){
-        const {name}=data;
-        const {icon,description}=data.weather[0];
-        const {temp,feels_like}=data.main;
-        const {humidity}=data.main;
-        const {speed}=data.wind;
-        document.querySelector(".city").innerText="weather in "+name;
-        document.querySelector(".icon").src="https://openweathermap.org/img/wn/" + icon + ".png";
-        document.querySelector(".description").innerText=description;
-        document.querySelector(".temp").innerText=temp+"°C";
-        document.querySelector(".feel").innerText="feels like="+feels_like+"°C";
-        document.querySelector(".humidity").innerText="humidity="+humidity+"%";
-        document.querySelector(".wind").innerText="wind speed="+speed+"km/h";
+let weather = {
+    api: "337e822ad82ee3693661794c6ca440b2",
+    fetchweather: async function (city) {
+        this.showLoading(); // Show loading indicator
+        try {
+            const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.api);
+            if (!response.ok) {
+                throw new Error("City not found");
+            }
+            const data = await response.json();
+            this.displayweather(data);
+        } catch (error) {
+            alert(error.message);
+        } finally {
+            this.hideLoading(); // Hide loading indicator
+        }
+    },
+    displayweather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, feels_like } = data.main;
+        const { humidity } = data.main;
+        const { speed } = data.wind;
+
+        // Clear previous results
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".feel").innerText = "Feels like: " + feels_like + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
     },
     search: function () {
-        weather.fetchweather(document.querySelector(".search-bar").value)
+        const city = document.querySelector(".search-bar").value;
+        if (city) {
+            this.fetchweather(city);
+        } else {
+            alert("Please enter a city name.");
+        }
+    },
+    showLoading: function () {
+        document.querySelector(".city").innerText = "Loading...";
+        document.querySelector(".icon").src = ""; // Clear previous icon
+    },
+    hideLoading: function () {
+        // You can add any additional UI logic here
     }
-}
-document.querySelector(".button").addEventListener("click",function (){
-    weather.search()
-})
+};
+
+document.querySelector(".button").addEventListener("click", function () {
+    weather.search();
+});
+
+// Allow Enter key to trigger the search
+document.querySelector(".search-bar").addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        weather.search();
+    }
+});
